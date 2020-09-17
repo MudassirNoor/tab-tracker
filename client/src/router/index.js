dc094660d49093db9dc26e37e3399d039ae74fc7
MudassirNoor <mnoor@sfu.ca>
2020-09-17 00:05:51 -0700

    minor fix in app.js and added sample router setup on the client side

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Register from '@/components/Register' // Import the Register component
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/', // Base path http://localhost:8080/#/
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    { // create a register route http://localhost:8080/#/register
+      path: '/register',
+      name: 'register',
+      component: Register
     }
   ]
 })
