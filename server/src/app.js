8ada40f805914722f048bde2fee39a0bcb410c9e
MudassirNoor <mnoor@sfu.ca>
2020-09-18 00:25:36 -0700

    initial setup of database and sample modulariztion of app.js using config

--- server/src/app.js
+++ server/src/app.js
@@ -2,25 +2,19 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+const {sequelize} = require('./models')
+const config = require('./config/config')
 
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-//'status' endpoint
-app.get('/status', (req, res) => { //Get request to a 'status' endpoint, here get is the method being called
-  res.send({
-    message: 'hello world!'
-  })
-})
-
 //'register' endpoint
-app.post('/register', (req, res) => {
-  res.send({
-    message: `Hello ${req.body.email}! Your user was registered! Have fun!`
-  })
-})
+require('./routes')(app)
 
-//Try entering localhost:8081/status in the browser
-app.listen(process.env.PORT || 8081)
+sequelize.sync()
+  .then(() =>  {
+    app.listen(config.port) //similar to process.env.PORT || 8081)
+    console.log(`Sequelize started on port ${config.port}`)
+  })
---
+++ server/src/models/index.js
@@ -0,0 +1,30 @@
+const fs = require('fs') // node way of accessing the file system
+const path = require('path') // used for path functionalities like relative path, joining path, direct path etc
+const Sequelize = require('sequelize')
+const config = require('../config/config')
+const db = {}
+
+// Create a sequelize object with our database configuration
+const sequelize = new Sequelize(
+  config.db.database,
+  config.db.user,
+  config.db.password,
+  config.db.options
+)
+
+// This will allow us to setup models easily
+fs
+  .readdirSync(__dirname) // read our current directory
+  .filter((file) => // filter out the index file
+  file !== 'index.js'
+  )
+  .forEach((file) => {
+    // for each file we will declare a model and name the model
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes)
+    db.file = model
+  })
+
+db.sequelize = sequelize
+db.Sequelize = Sequelize
+
+module.exports = db
