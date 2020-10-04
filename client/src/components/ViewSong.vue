34dc575089cb18cf169ffec3ee64f1ad796b2958
MudassirNoor <mnoor@sfu.ca>
2020-10-04 01:28:47 -0700

    created layout for ViewSong component - metadata - tabs - youtube video - lyrics

--- client/src/components/ViewSong.vue
+++ client/src/components/ViewSong.vue
@@ -1,34 +1,51 @@
 <!-- This will allow to create a template for v-toolbar we have created in all three pages-->
 <!-- The slot section is where we include the customization for each page within the Panel tag in those pages.-->
 <template>
-  <v-layout>
-    <v-flex xs6>
-      <panel title="Song Metadata">
-        <br>
-        <v-layout>
-          <v-flex xs6>
-            <div class="song-title">
-              {{song.title}}
-            </div>
-            <div class="song-artist">
-              {{song.artist}}
-            </div>
-            <div class="song-album">
-              {{song.album}}
-            </div>
-          </v-flex>
-          <v-flex xs6>
-            <img class="song-album-image" :src="song.albumImageURL">
-          </v-flex>
-        </v-layout>
-      </panel>
-    </v-flex>
-    <v-flex xs6 class="ml-2">
-      <panel title="Tabs">
-        {{song.tab}}
-      </panel>
-    </v-flex>
-  </v-layout>
+  <div>
+    <v-layout>
+      <v-flex id="SongMetadata" xs6>
+        <panel title="Song Metadata">
+          <br>
+          <v-layout>
+            <v-flex xs6>
+              <div class="song-title">
+                {{song.title}}
+              </div>
+              <div class="song-artist">
+                {{song.artist}}
+              </div>
+              <div class="song-album">
+                {{song.album}}
+              </div>
+            </v-flex>
+            <v-flex xs6>
+              <img class="song-album-image" :src="song.albumImageURL">
+            </v-flex>
+          </v-layout>
+        </panel>
+      </v-flex>
+      <v-flex id="Tabs" xs6 class="ml-2">
+        <panel title="Tabs">
+          <textarea readonly v-model="song.tab">
+          </textarea>
+        </panel>
+      </v-flex>
+    </v-layout>
+    <v-layout>
+      <v-flex id="YoutubeVideo" xs6>
+        <panel title="Youtube Video">
+        <textarea readonly v-model="song.lyrics">
+        </textarea>
+        </panel>
+      </v-flex>
+      <v-flex id="Lyrics" xs6 class="ml-2">
+        <panel title="Lyrics">
+        <textarea readonly v-model="song.lyrics">
+        </textarea>
+        </panel>
+      </v-flex>
+    </v-layout>
+  </div>
 </template>
 
 <script>
@@ -69,5 +86,14 @@ export default {
 }
 .song-artist {
   font-size: 24px;
+}
+
+textarea {
+  width: 80%;
+  font-family: monospace;
+  height: 330px;
+  border: none transparent;
+  overflow: auto;
+  padding: 40px;
 }
 </style>
